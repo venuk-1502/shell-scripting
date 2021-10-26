eaa453578f12b58da932c1363800b56a9de25a79
venuk-1502 <koonamneni1502@gmail.com>
2021-10-26 22:03:24 +0530

    test commit

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -14,12 +14,14 @@ else
   echo "There is an instance already with the name: $1"
 fi
 
-
 ip_address=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null|xargs)
 echo "Instance Private IP Address: $ip_address"
 sed -e "s/DNS_NAME/$1.knowaws.com/" -e "s/IP_ADDRESS/${ip_address}/" change-resource-record-sets.json > /tmp/change-resource-record-sets.json
-aws route53 change-resource-record-sets --hosted-zone-id Z00216652JEVANUOGF0R3 --change-batch file:///tmp/change-resource-record-sets.json | jq
-
+aws route53 change-resource-record-sets --hosted-zone-id Z00216652JEVANUOGF0R3 --change-batch file:///tmp/change-resource-record-sets.json | jq >> /dev/null
+if [ $? -eq 0 ]; then
+  echo "Successfully created DNS name for new instance: $1.knowaws.com"
+else:
+  echo "Route53 DNS record creation failed."
 #--launch-specification file://specification.json
 
 #--tag-specifications 'ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]'
