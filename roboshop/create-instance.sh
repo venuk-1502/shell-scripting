1175ad77a590e4bcecdb1af6bf131f873efefdd9
venuk-1502 <koonamneni1502@gmail.com>
2021-10-26 18:13:35 +0530

    test commit

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -5,7 +5,7 @@
 #--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$1}]'
 
 aws ec2 request-spot-instances --spot-price "0.0036" --instance-count 1 --type "persistent" --image-id ami-0e4e4b2f188e91845 --instance-type t2.micro \
---security-group-ids sg-0ec7a1055abb5cf0c --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$1}]'
+--security-group-ids sg-0ec7a1055abb5cf0c --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$1}]' | jq
 
 #--launch-specification file://specification.json
 
